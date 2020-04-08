94a1b0e3ea9eaf4fee9854af8f5a1c6f18391b5b
Fisher Heins <fisher@heinsdesign.com>
2020-04-08 14:13:49 -0500

    Basic click and drag and rotation

---
+++ Assets/Scripts/Drag.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour
+{
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    [SerializeField]
+    float rotationAmount = 1f;
+
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + offset;
+        transform.position = cursorPosition;
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(new Vector3(0, 0, 1), -rotationAmount);
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(new Vector3(0, 0, 1), rotationAmount);
+        }
+    }
+}
