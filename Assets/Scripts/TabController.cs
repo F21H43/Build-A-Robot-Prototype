b633764ef25447e7c6f3af3cfea2f1eacf3383ca
grantfreyBU <54907819+grantfreyBU@users.noreply.github.com>
2020-04-15 19:36:04 -0400

    New UI Functionallity is in

---
+++ Assets/Scripts/TabController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabController : MonoBehaviour
+{
+    public List<GameObject> tabWindows;
+
+    public void OpenTabWindow(int tab)
+    {
+        for(int i = 0; i < tabWindows.Capacity; i++)
+        {
+            if(i != tab)
+            {
+                tabWindows[i].SetActive(false);
+            }
+            else
+            {
+                tabWindows[i].SetActive(true);
+            }
+        }
+    }
+
+    public void OpenBlueprintTabWindow(int tab)
+    {
+        for (int i = 0; i < tabWindows.Capacity; i++)
+        {
+            if (i != tab)
+            {
+                tabWindows[i].SetActive(false);
+            }
+            else if (i == tab && tabWindows[i].activeSelf)
+            {
+                tabWindows[i].SetActive(false);
+            }
+            else
+            {
+                tabWindows[i].SetActive(true);
+            }
+        }
+    }
+}
