b25f96620df1b1c52afbd04b3f1e091b225e8ce5
grantfreyBU <54907819+grantfreyBU@users.noreply.github.com>
2020-04-18 17:52:20 -0400

    Info panel is in

---
+++ Assets/Scripts/MoreInfoPanelController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoreInfoPanelController : MonoBehaviour
+{
+    public GameObject panel;
+    public Text nameText;
+    public Image partImage;
+    public Text powerText;
+    public Text partDescription;
+    public Button closePanel;
+
+    public void OpenMoreInfoPanel()
+    {
+        panel.SetActive(true);
+    }
+
+    public void CloseMoreInfoPanel()
+    {
+        panel.SetActive(false);
+    }
+
+    public void SetName(string name)
+    {
+        nameText.text = name;
+    }
+
+    public void SetImage(Sprite image)
+    {
+        partImage.sprite = image;
+    }
+
+    public void SetPowerText(string text)
+    {
+        powerText.text = text;
+    }
+
+    public void SetPartDescription(string description)
+    {
+        partDescription.text = description;
+    }
+}
