24d692d6b483a70633beec8488eac03b87a05478
grantfreyBU <54907819+grantfreyBU@users.noreply.github.com>
2020-04-20 11:41:51 -0400

    Dragging from button to spawn works properly now

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -9,8 +9,14 @@ public class Drag : MonoBehaviour
     public static float rotationAmount = 1f;
     public bool isHeld;
 
+    private void Start()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+    }
+
     private void Update()
     {
+        FollowMouseCursor();
         if (transform.position.x < 0 && !isHeld)
         {
             if (this.gameObject.layer == 8)
@@ -19,6 +25,11 @@ public class Drag : MonoBehaviour
             }
             Destroy(this.gameObject);
         }
+        if(Input.GetMouseButtonUp(0) && isHeld)
+        {
+            isHeld = false;
+        }
+        
     }
 
     void OnMouseDown()
@@ -29,22 +40,24 @@ public class Drag : MonoBehaviour
 
     void OnMouseDrag()
     {
-        isHeld = true;
-        Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-        Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + offset;
-        transform.position = cursorPosition;
-        if (Input.GetKey(KeyCode.E))
-        {
-            transform.Rotate(new Vector3(0, 0, 1), -rotationAmount);
-        }
-        else if (Input.GetKey(KeyCode.Q))
-        {
-            transform.Rotate(new Vector3(0, 0, 1), rotationAmount);
-        }
+        isHeld = true;       
     }
 
-    private void OnMouseUp()
+    private void FollowMouseCursor()
     {
-        isHeld = false;
+        if (isHeld)
+        {
+            Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+            Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + offset;
+            transform.position = cursorPosition;
+            if (Input.GetKey(KeyCode.E))
+            {
+                transform.Rotate(new Vector3(0, 0, 1), -rotationAmount);
+            }
+            else if (Input.GetKey(KeyCode.Q))
+            {
+                transform.Rotate(new Vector3(0, 0, 1), rotationAmount);
+            }
+        }
     }
 }
