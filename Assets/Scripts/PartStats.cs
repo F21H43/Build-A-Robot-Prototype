cbfae1673363bd69220146fdb2c9f34e045596e8
grantfreyBU <54907819+grantfreyBU@users.noreply.github.com>
2020-04-20 11:59:46 -0400

    Added basis for power system

---
+++ Assets/Scripts/PartStats.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartStats : MonoBehaviour
+{
+    public int powerUsage;
+    public int powerProduction;
+    RobotStats rs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rs = FindObjectOfType<RobotStats>();
+        rs.powerProduced += powerProduction;
+        rs.powerUsed += powerUsage;
+    }
+
+    private void OnDestroy()
+    {
+        rs.powerProduced -= powerProduction;
+        rs.powerUsed -= powerUsage;
+    }
+}
